254c5b2a0d5925ed5593e58e18ea509deee4c11d
Austin Ankney <austinankney@Austins-MacBook-Pro.local>
2015-03-05 07:10:59 -0500

    replaced sqlite with postgres in the backend. Ready to deploy to heroku.

--- webapps/settings.py
+++ webapps/settings.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read("config.ini")
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -59,10 +64,20 @@ LOGIN_REDIRECT_URL = "/frontPage"
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config.get('PGSQL', 'Name'),
+        'USER': config.get('PGSQL', 'User'),
+        'PASSWORD': '',
+        'HOST':''
     }
 }
 
